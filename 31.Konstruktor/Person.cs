28b1d9b8200d3b7f09c81bf58aeb2d75d27999ec
Michał Przybyłowski <myss1990@gmail.com>
2023-04-23 10:33:55 +0200

    Add project files.

---
+++ 31.Konstruktor/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30.Class_method
+{
+    internal class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        private DateTime dateOfBirth;
+
+        public Person(string firstName, string lastName) // pusty konstruktor(incjacja wartości danego oobiektu) jest tworzony domyślnie, można tworzyć przypisane by sprawdzic wartości w nim przypisane
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        } 
+        public void SetDateOfBirth(DateTime date) // można dzięki temu zmodyfikować dateOfBirth tylko dzięki tej metodzie
+        {
+            if (date > DateTime.Now)
+            {
+                Console.WriteLine("Invalid date of  birth");
+            }
+            else
+            {
+                dateOfBirth = date;
+            }
+        }
+        public DateTime GetDateOfBirth() // => dateOfBirth (można tak to zapisać bez nawiasów klamrowych), trzeba to utworzyć, żeby mieć do tego dostęp poza klasą
+        {
+            return dateOfBirth;
+        }
+        public void SayHi() // gdybyśmy mieli dużo do wpisania danych typu person to możemy użyć funkcji jak poniżej, żeby każdego "przywitać"
+        {
+            Console.WriteLine($"Hi, I'm {this.FirstName}, {GetDateOfBirth()}"); // this.Zmienna to może być pomocne gdy mamy argument w metodzie tak samo nazwany jak pole w klasie
+        }
+    }
+}
---
+++ 31.Konstruktor/Program.cs
@@ -0,0 +1,20 @@
+﻿using _30.Class_method;
+using System;
+
+namespace Program
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person bill = new Person("Johny","Wick"); // konstruktor
+
+            bill.SetDateOfBirth(new DateTime(1990, 1, 1));
+
+            Console.WriteLine($"{bill.FirstName} {bill.GetDateOfBirth()}");
+
+            bill.SayHi(); // wywolania funkcji SayHi
+        }
+    }
+}
